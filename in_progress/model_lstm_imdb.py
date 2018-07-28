52989babf6dbed2e1657315b4af0aab4e145280d
vivekverma239 <vivekverma239@gmail.com>
2018-07-28 22:51:53 +0530

    add

--- in_progress/model_lstm_imdb.py
+++ in_progress/model_lstm_imdb.py
@@ -4,7 +4,7 @@ Code to classify sentiments
 '''
 
 
-from __future__ import absolute_import
+# from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from utils import DataProcessor
